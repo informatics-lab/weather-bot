62583cff99035fa3ec22efee8434d799d3d18df7
Thomas Powell <t.powell.meto@gmail.com>
2017-04-05 09:14:23 +0100

    persona configuration

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,14 +1,17 @@
 "use strict";
 
 // dependencies
-var nconf = require("nconf");
 var restify = require("restify");
-var request = require("request");
 var builder = require("botbuilder");
-var winston = require("winston");
 
 // application conf
+var nconf = require("nconf");
 var config = nconf.env().argv().file({file: "secrets.json"});
+config.app = require("../../../package.json");
+config.persona = config.get("PERSONA") ? config.get("PERSONA").toLowerCase() : "default";
+
+//logging conf
+var winston = require("winston");
 winston.configure({
     transports: [
         new (winston.transports.Console)({
@@ -18,35 +21,36 @@ winston.configure({
         })
     ]
 });
-var app = require("../../../package.json");
 
-var domains = require("./domains");
-var persona = require("./persona")(require("../resources/personas/default.json"));
 
 /*
  * application entry point
  */
 function main() {
-    winston.info("starting %s %s", app.name, app.version);
+    winston.info("starting %s %s", config.app.name, config.app.version);
 
     // Set up the bot server..
-    var server = restify.createServer({name:app.name});
+    var server = restify.createServer({name: config.app.name});
     server.use(restify.bodyParser({mapParams: false}));
     server.listen(config.get("PORT") || 3978, function () {
-        winston.info('%s listening on %s', server.name, server.url);
+        winston.info("%s listening on %s", server.name, server.url);
     });
     var connector = new builder.ChatConnector({
         appId: config.get("MICROSOFT_APP_ID"),
         appPassword: config.get("MICROSOFT_APP_PASSWORD")
     });
-    server.post('/api/messages', connector.listen());
-    
+    server.post("/api/messages", connector.listen());
+
     var bot = new builder.UniversalBot(connector, {persistConversationData: true});
 
     var recognizer = new builder.LuisRecognizer(config.get("LUIS_MODEL_URL"));
     bot.recognizer(recognizer);
 
+    var domains = require("./domains");
+    var persona = require("./persona")(require("../resources/personas/" + config.persona + ".json"));
+
+    //add domains here
     domains.general.greeting(bot, persona);
 
-};
+}
 main();
