4e804d427686b6f68da0e3ae3c84a05468862e59
Thomas Powell <t.powell.meto@gmail.com>
2017-04-18 16:42:29 +0100

    fix never ending repeat bug

--- src/main/js/intents/utils/index.js
+++ src/main/js/intents/utils/index.js
@@ -3,7 +3,7 @@
 exports.storeAsPreviousIntent = (session) => {
     if (session.sessionState) {
         if (session.sessionState.callstack && session.sessionState.callstack.length >= 2) {
-            var previousIntent = session.sessionState.callstack[1].id.substr(2);
+            var previousIntent = session.sessionState.callstack[session.sessionState.callstack.length - 1].id.substr(2);
             session.conversationData.previous_intent = previousIntent;
         }
         session.endDialog();
